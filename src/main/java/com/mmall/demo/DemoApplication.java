cb0c365e0f2fdb03ce3d4ef20413f20cad709867
jiyifa <jiyifa2017@126.com>
2019-01-03 00:04:17 +0800

    Initial commit

---
+++ src/main/java/com/mmall/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.mmall.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	@RequestMapping("/")
+	public String home(){
+		return "Hello Spring Boot";
+	}
+	@RequestMapping("/hello")
+	public String hello(){
+		return "Hello world";
+	}
+}
+
